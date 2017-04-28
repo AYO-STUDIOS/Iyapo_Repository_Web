d49be7faa6778644a65a205b6379bfb562c4afca
Mischa Abakumova <mischa.abakumova@gmail.com>
2017-04-28 11:08:58 -0400

    p5 structure

---
+++ particles_system/particles_systems.js
@@ -0,0 +1,28 @@
+
+var system;
+
+
+function setup() {
+	// create a place to draw
+	createCanvas(windowWidth, windowHeight);
+}
+
+
+function draw() {
+	// clear the background
+	background(0, 0, 0);
+
+	// set drawing styles
+	stroke(255, 0, 0);
+	fill(255, 255, 255);
+	strokeWeight(10);
+
+	// draw a circle
+	ellipse(320, 180, 100, 100);
+}
+
+
+// Single particle class
+// Class Methods
+
+
