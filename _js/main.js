b1ae78e01ad771eae1c54f9e34b76f80788f0871
Mischa Abakumova <mischa.abakumova@gmail.com>
2017-05-19 01:02:26 -0400

    Implements a toggle between two states. Object indexing fix.

--- _js/main.js
+++ _js/main.js
@@ -1,88 +1,86 @@
 
-var modal = document.getElementById('aboutModal');
-var contactModal = document.getElementById('contactModal');
-var btn = document.getElementById("myAbout");
-var btn2 = document.getElementById("myContact");
+// CLASSIC
 
+if(document.URL.indexOf("static.html") >= 0){ 
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  console.log('you are in the classic mode');
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
+  var modal = document.getElementById('aboutModal');
+  var contactModal = document.getElementById('contactModal');
+  var btn = document.getElementById("myAbout");
+  var btn2 = document.getElementById("myContact");
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal 
+  btn.onclick = function() {
     modal.style.display = "block";
-}
-btn2.onclick = function() {
+  }
+  btn2.onclick = function() {
     contactModal.style.display = "block";
-}
+  }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
     modal.style.display = "none";
     consol.log("i'm pressing on you damn");
-}
-span.onclick = function() {
+  }
+  span.onclick = function() {
     contactModal.style.display = "none";
-}
+  }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
         if (event.target == contactModal) {
         contactModal.style.display = "none";
     }
-}
+  }
 
-var objects = [];
+  var objects = [];
 
-// creating 6 arrays in our orig. array
-for(var i = 0; i < 6; i++){
+  // creating 6 arrays in our orig. array
+  for(var i = 0; i < 6; i++){
    objects[i] = [Math.random(), Math.random()] 
-}
+  }
 
-for(i in objects){
+  for(i in objects){
   for(n in objects[i]){
   }
-}
+  }
 
-$('#burger').hover(function() {
+  $('#burger').hover(function() {
   $(this).find('#hidden-nav').fadeIn();
-});
-$('#burger').mouseleave(function() {
+  });
+  $('#burger').mouseleave(function() {
   $(this).find('#hidden-nav').fadeOut();
-});
+  });
 
-var countItems = document.getElementsByTagName("ul")[0].childElementCount;
-console.log('countItems: ' + countItems);
+  var countItems = document.getElementsByTagName("ul")[0].childElementCount;
+  console.log('countItems: ' + countItems);
 
 
-$(document).ready(function() {
+  $(document).ready(function() {
 
     $("#repo ul li").click(function() {
         var index = $("#repo ul li").index(this);
         console.log('That was div index #' + index);
     });
-   
-});
+
+  });
+}
 
 
-// $('.repo ul li').click(function(){
-//     alert( $('.repo ul li').index(this) );
-//     console.log('That was div index #' + index);
-// });
+// EXPLORATION
 
-
-// document.getElementById('toggle').addEventListener("change", function(){
-//         console.log('toggle checked');
-//         if (this.checked) window.location = "index.html";
-//     });
-
-
-// $('#toggle').click(function() {
-//   console.log("home pressed")
-//   window.location = "index.html";
-// });
+if(document.URL.indexOf("index.html") >= 0){ 
+  console.log('you are in the exploration mode');
+  $('#toggle').change(function(){
+    $('#toggle').toggle(this.checked); 
+    console.log('toggle checked');
+    window.location = "static.html";
+  });
+}
 
 
